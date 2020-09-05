f76cdb06d484564840e07d1f94b262df2775ab12
Nicole Tilbe <nicolecali2406@outlook.com>
2020-09-05 15:48:59 -0400

    finished MVP tasks + stretch

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
+import { removeFeature } from '../actions/featureActions'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button is-danger" onClick={() => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { initialState } from '../reducers/featuresReducer'
+
+import { addFeature } from '../actions/featureActions'
+
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button is-primary" onClick={() => props.addFeature(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, { addFeature })(AdditionalFeature);
